ba272c6938be3034469957cf8492f362a7b7762e
talha <baigtalha7@gmail.com>
2024-06-26 10:37:41 +0500

    create get,post,delete,patch API using express.js with dummy data

---
+++ index.js
@@ -0,0 +1,70 @@
+import express from "express";
+import users from "./MOCK_DATA (1).json" assert { type: "json" };
+import fs from "fs";
+const app = express();
+const port = 6400;
+app.use(express.urlencoded({ extended: false }));
+
+// how to send html tang on responce
+// app.get("/users", (req, res) => {
+//     const html = `<ui>
+//       ${users.map((user) => `<li>${user.gender}</li>`).join("")}
+//       </ui>`;
+//     res.send(html);
+//   });
+
+// 1 : GET /users -  get all user data
+
+app.get("/users", (req, res) => {
+  return res.json(users);
+});
+
+app.post("/users", (req, res) => {
+  const body = req.body;
+
+  users.push({ ...body, id: users.length + 1 });
+  fs.writeFile("./MOCK_DATA (1).json", JSON.stringify(users), (err, data) => {
+    return res.json({ status: "success" });
+  });
+});
+
+// 2 GET /users/id -  get specific user with url id
+
+// and i make it grouping
+
+app
+  .route("/users/:id")
+  .get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+
+    return res.send(user);
+  })
+  .patch((req, res) => {
+    const id = Number(req.params.id);
+    const index = users.findIndex((user) => user.id === id);
+
+    users[index] = { ...users[index], ...req.body };
+    
+
+    fs.writeFile("./MOCK_DATA (1).json", JSON.stringify(users), (err) => {
+      if (err) {
+        console.log("error");
+      } else {
+        return res.json({ status: "success", user: users[index] });
+      }
+    });
+  })
+  .delete((req, res) => {
+    const id = Number(req.params.id);
+
+    const index = users.findIndex((user) => user.id === id);
+    users.splice(index, 1);
+    fs.writeFile("./MOCK_DATA (1).json", JSON.stringify(users), (err) => {
+      if (err) {
+        console.log("error");
+      } else return res.json({ status: "Success" });
+    });
+  });
+
+app.listen(port, () => console.log(`app listen on port ${port}`));
